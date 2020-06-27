4720f623bf25cb01182c8e92d70c4d29d2b25130
ilein <ileinoriana@gmail.com>
2020-06-26 21:37:08 -0300

    Initial commit

---
+++ principiante.py
@@ -0,0 +1,39 @@
+import random
+
+#Tirar un dado de 5 valores 
+#Si es 1, gana una flor. 
+#Si es 2, gana un tatuaje. 
+#Si es 3, gana un libro. 
+#Si es 4, gana un viaje. 
+#Si es 5, no gana nada. 
+
+def lanzar_dados():
+    value = random.randint(1, 5)
+    return value
+    
+
+message = {
+    1: lambda  : print('ganaste una flor'),
+    2: lambda  : print('ganaste un tatuaje'),
+    3: lambda  : print('ganaste un libro'),
+    4: lambda  : print('ganaste un viaje'),
+    5: lambda  : print('no ganaste nada')
+}
+
+
+def descifrar_premio(num):
+    message[num]()
+    # if num == 1:
+    #     print('ganaste una flor')       
+    # elif num == 2: 
+    #     print('ganaste un tatuaje')
+    # elif num == 3: 
+    #     print('ganaste un libro')
+    # elif num == 4: 
+    #     print('ganaste un viaje')
+    # else: 
+    #     print('no ganaste nada')
+
+dado = lanzar_dados()
+print('tu dado es ', dado)
+descifrar_premio(dado)
